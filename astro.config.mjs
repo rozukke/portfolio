8dbabcdbafbda6f6ae9a85c275ef4f31621ae9ec
Artemis Rosman <73006620+rozukke@users.noreply.github.com>
2025-07-15 21:16:28 +1000

    feat: update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import expressiveCode from 'astro-expressive-code';
 // https://astro.build/config
 export default defineConfig({
   site: "https://rozukke.dev",
-  integrations: [sitemap(), mdx()]
+  integrations: [
+    sitemap(),
+    expressiveCode({
+      themes: ["dracula-soft"],
+    }),
+    mdx()
+  ]
 });
